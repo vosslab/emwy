6d4db2e8516f78ebace8b997cd639ffd34389419
Neil Voss <vosslab@Mac-Studio-36G.local>
2025-12-19 14:44:59 -0600

    v2 samples

---
+++ samples/runGangnam_v2.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${SCRIPT_DIR}"
+
+if [ ! -f "Psy-Gangnam_Style.mp4" ]; then
+	yt-dlp -f 18 http://youtu.be/9bZkp7q19f0 -o Psy-Gangnam_Style.mp4
+fi
+
+../emwy.py -y gangnam.emwy.yaml
